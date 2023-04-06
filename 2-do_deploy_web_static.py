5e8cae90b8f62b4377d25db75324db19db72b408
dukeethio <dukeethiopian@gmail.com>
2023-04-06 01:49:05 -0700

    hot fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,50 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+"""
+Fabric script that distributes an archive to your web servers, using the
+function do_deploy.
+"""
+
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import *
+from fabric.operations import run, put, sudo
 
-env.hosts = ["54.157.32.137", "52.55.249.213"]
-
+env.hosts = ['52.55.249.21', '54.157.32.137']
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+    """Distributes an archive to the web servers."""
+    if not os.path.isfile(archive_path):
+        return False
 
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension>
+        archive_filename = os.path.basename(archive_path)
+        archive_basename = os.path.splitext(archive_filename)[0]
+        remote_path = "/data/web_static/releases/{}".format(archive_basename)
+        run("sudo mkdir -p {}".format(remote_path))
+        run("sudo tar -xzf /tmp/{} -C {}".
+            format(archive_filename, remote_path))
+
+        # Delete the archive from the web server
+        run("sudo rm /tmp/{}".format(archive_filename))
+
+        # Move contents of web_static to /data/web_static/releases/
+        run("sudo mv {}/web_static/* {}/".format(remote_path, remote_path))
+        run("sudo rm -rf {}/web_static".format(remote_path))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("sudo rm -rf /data/web_static/current")
+
+        # Create a new the symbolic link /data/web_static/current on the web server
+        # linked to the new version of your code
+        run("sudo ln -s {} /data/web_static/current".format(remote_path))
+
+        print("New version deployed!")
+        return True
+
+    except Exception:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True 
+ 
